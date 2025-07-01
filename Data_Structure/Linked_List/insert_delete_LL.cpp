48570d0a6e60cc17e1ff617efd0895052ac6edfb
Tushar Goyal <90262839+TusharSahib@users.noreply.github.com>
2025-07-01 17:06:43 +0000

    update

--- Data_Structure/Linked_List/insert_delete_LL.cpp
+++ Data_Structure/Linked_List/insert_delete_LL.cpp
@@ -54,9 +54,35 @@ Node* deleteTail(Node* head){
     return head;
 }
 
+Node* deleteK(Node* head,int k){
+    if(head==NULL) return head;
+
+    if(k==1){
+        Node* temp=head;
+        head=head->next;
+        delete temp;
+        return head;
+    }
+    int cnt=0;
+    Node* temp=head;
+    Node* prev=NULL;
+    while(temp!=NULL){
+        cnt++;
+        if(cnt==k){
+            prev->next=prev->next->next;
+            delete temp;
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+    }
+    return head;
+}
+
 int main(){
     vector<int> arr={1,2,3,4,5};
     Node* head=ConvertArrToLL(arr);
-    head=deleteHead(head);
+    // head=deleteHead(head);
+    deleteK(head,3);
     printLL(head);
 }
